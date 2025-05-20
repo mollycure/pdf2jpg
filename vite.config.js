1909ec391e5a7a8d574c2d195dc0721e7ec240cf
leduy <3hinney@gmail.com>
2025-05-20 22:14:04 +0700

    fixed bug

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'esnext'
+    }
+  },
+  build: {
+    target: 'esnext'
+  },
   plugins: [react()],
 })
